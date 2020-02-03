13d03e8b248b9ab7fc324f31dc0a3ec37b3838bf
t-kowalewski <wkowalewski@outlook.com>
2020-02-03 11:28:44 +0100

    change 'question' variable to const

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
+    const questions = [
       {
         'questionText': 'What\'s your favorite color?',
         'answer': ['Red', 'Green', 'Yellow', 'Blue']
